2073860931aaabed200095ab231448b2edd5a9fc
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-10-05 10:51:13 +0600

    learn: takeing input and output by variable

--- module-01/module-1.c
+++ module-01/module-1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main() {
-	printf("Hello\n");
-	printf("Helee Bhaiy");
+	int n1;
+	float n2;
+	scanf("%d",&n1);
+	scanf("%f",&n2);
+	// int num = 10;
+	// float f = 10.20;
+	// char c = 'd';
+	// bool t = false;
+	// printf("%d\n",num );
+	// printf("%f\n",f);
+	// printf("%c\n",c);
+	// printf("%b\n",t);
+	printf("%d %.4f",n1,n2);
 };

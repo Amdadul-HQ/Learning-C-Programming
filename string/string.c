f47b7c4929495824defb05a745d07b493f2d0e8b
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-10-22 06:27:42 +0600

    add: way to long problem solve

--- string/string.c
+++ string/string.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
 int main(){
+
+	// Reverse arrr
+	// int n;
+	// scanf("%d",&n);
+	// int ar[n];
+	// for (int i = 0; i < n; i++)
+	// {
+	// 	/* code */
+	// 	scanf("%d",&ar[i]);
+	// }
+	// int j = n-1;
+
+	// for (int i = 0; i < j; i++)
+	// {
+	// 	/* code */
+	// 	int tmp = ar[i];
+	// 	ar[i] = ar[j];
+	// 	ar[j] = tmp;
+	// 	j--;
+	// }
+	// for (int i = 0; i < n; i++)
+	// {
+	// 	/* code */
+	// 	printf("%d ",ar[i]);
+	// }
+
+	// Way too long
 	int n;
 	scanf("%d",&n);
-	int ar[n];
-	for (int i = 0; i < n; i++)
-	{
-		/* code */
-		scanf("%d",&ar[i]);
-	}
-	int j = n-1;
+	char s[101];
+    while (scanf("%s",s) != EOF) {
+        
+        int count=0;
+        for (int i = 0; s[i] !='\0'; i++)
+        {
+        	/* code */
+        	count++;
+        }
 
-	for (int i = 0; i < j; i++)
-	{
-		/* code */
-		int tmp = ar[i];
-		ar[i] = ar[j];
-		ar[j] = tmp;
-		j--;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		/* code */
-		printf("%d ",ar[i]);
-	}
+        if(count<=n){
+        	printf("%s\n",s);
+        }
+        else{
+        	int a = count -2;
+        	char first = s[0];
+        	char last = s[count-1];
+        	printf("%c%d%c\n",first,a,last);
+        }
+    }
 }

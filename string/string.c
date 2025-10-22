7911e6881a4421eda175f70f5a13626ed97e6c4a
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-10-22 06:47:41 +0600

    conversion problem solve

--- string/string.c
+++ string/string.c
@@ -27,26 +27,47 @@ int main(){
 	// }
 
 	// Way too long
-	int n;
-	scanf("%d",&n);
-	char s[101];
-    while (scanf("%s",s) != EOF) {
+	// int n;
+	// scanf("%d",&n);
+	// char s[101];
+    // while (scanf("%s",s) != EOF) {
         
-        int count=0;
-        for (int i = 0; s[i] !='\0'; i++)
-        {
-        	/* code */
-        	count++;
-        }
+    //     int count=0;
+    //     for (int i = 0; s[i] !='\0'; i++)
+    //     {
+    //     	/* code */
+    //     	count++;
+    //     }
 
-        if(count<=n){
-        	printf("%s\n",s);
-        }
-        else{
-        	int a = count -2;
-        	char first = s[0];
-        	char last = s[count-1];
-        	printf("%c%d%c\n",first,a,last);
+    //     if(count<=n){
+    //     	printf("%s\n",s);
+    //     }
+    //     else{
+    //     	int a = count -2;
+    //     	char first = s[0];
+    //     	char last = s[count-1];
+    //     	printf("%c%d%c\n",first,a,last);
+    //     }
+    // }
+
+    // conversion
+   char s[100001];
+    scanf("%s", s);
+
+    // Loop through the string
+    for (int i = 0; s[i] != '\0'; i++) {
+
+        if (s[i] == ',') {
+            s[i] = ' ';  // Replace comma with space
+        } 
+        else if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] = s[i] - 32;  // Convert lowercase → uppercase
+        } 
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] = s[i] + 32;  // Convert uppercase → lowercase
         }
     }
+
+    printf("%s\n", s);
+    return 0;
 }

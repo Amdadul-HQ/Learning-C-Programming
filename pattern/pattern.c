2b58b5d5e6c4151852d7d5252b31d4efe67cf3f3
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-01 20:23:51 +0600

    pattren c problem solving

--- pattern/pattern.c
+++ pattern/pattern.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main () {
-	
+
+	int n;
+	scanf("%d",&n);
+	for (int i = 0; i < n; i++)
+	{
+		/* code */
+		for (int a = 0; a <=i; a++)
+		{
+			/* code */
+			printf("*");
+		}
+		printf("\n");
+	}
 }

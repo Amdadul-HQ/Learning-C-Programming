ee0c3867e3922fda2f18e0db1bf211bf2c7bc817
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-02 12:45:14 +0600

    Patter reverse

--- pattern/pattern.c
+++ pattern/pattern.c
@@ -4,10 +4,10 @@ int main () {
 
 	int n;
 	scanf("%d",&n);
-	for (int i = 0; i < n; i++)
+	for (int i = n; i>=0 ; i--)
 	{
 		/* code */
-		for (int a = 0; a <=i; a++)
+		for (int a = 1; a<=i; a++)
 		{
 			/* code */
 			printf("*");

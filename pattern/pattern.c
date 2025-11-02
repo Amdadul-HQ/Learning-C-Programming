41062988261c5a2565d27af94c5a1d960d2e037f
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-02 13:17:40 +0600

    piramid pattern

--- pattern/pattern.c
+++ pattern/pattern.c
@@ -4,10 +4,18 @@ int main () {
 
 	int n;
 	scanf("%d",&n);
-	for (int i = n; i>=0 ; i--)
+	for (int i = 1; i<=n ; i++)
 	{
+	  int space = n-i;
+	  // printf("%d",space);
+	  for (int b = 1; b <=space; b++)
+			{
+				/* code */
+				printf(" ");
+			}
+
 		/* code */
-		for (int a = 1; a<=i; a++)
+		for (int a = 1; a<=(2 * i -1); a++)
 		{
 			/* code */
 			printf("*");

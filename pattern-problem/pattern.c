be707a14c7341aa47ee0377942a50673e99a3790
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-04 08:03:46 +0600

    sorting problem solve

--- pattern-problem/pattern.c
+++ pattern-problem/pattern.c
@@ -52,37 +52,65 @@ int main () {
 	//  *****
 	//   ***
 	//    *
+	// int n;
+	// scanf("%d",&n);
+
+	// for (int i = 1; i <=n; i++)
+	// {
+	// 	for (int a = 0; a < n-i; a++)
+	// 	{
+	// 		/* code */
+	// 		printf(" ");
+	// 	}
+	// 	/* code */
+	// 	for (int b = 1; b<=(2 * i -1); b++ )
+	// 	{
+	// 		/* code */
+	// 		printf("*");
+	// 	}
+	// 	printf("\n");	
+	// }
+	// for (int i = n; i > 0; i--)
+	// {
+	// 	for (int a = 0; a < n-i; a++)
+	// 	{
+	// 		/* code */
+	// 		printf(" ");
+	// 	}
+	// 	/* code */
+	// 	for (int b = 1; b<=(2 * i -1); b++ )
+	// 	{
+	// 		/* code */
+	// 		printf("*");
+	// 	}
+	// 	printf("\n");	
+	// }
+
+	// Sorting
 	int n;
 	scanf("%d",&n);
-
-	for (int i = 1; i <=n; i++)
+	int ar[n];
+	for (int i = 0; i < n; i++)
 	{
-		for (int a = 0; a < n-i; a++)
-		{
-			/* code */
-			printf(" ");
-		}
 		/* code */
-		for (int b = 1; b<=(2 * i -1); b++ )
-		{
-			/* code */
-			printf("*");
-		}
-		printf("\n");	
+	   scanf("%d",&ar[i]);
 	}
-	for (int i = n; i > 0; i--)
+	for (int i = 0; i < n-1; i++)
 	{
-		for (int a = 0; a < n-i; a++)
-		{
-			/* code */
-			printf(" ");
-		}
 		/* code */
-		for (int b = 1; b<=(2 * i -1); b++ )
+		for (int a = i+1; a <n ; a++)
 		{
 			/* code */
-			printf("*");
+			if(ar[i]>ar[a]){
+				int tmp = ar[i];
+				ar[i] = ar[a];
+				ar[a] = tmp;
+			}
 		}
-		printf("\n");	
+	}
+	for (int i = 0; i < n; i++)
+	{
+		/* code */
+		printf("%d ",ar[i]);
 	}
 }

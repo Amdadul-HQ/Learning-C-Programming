636f082e36050cb338f9c6ea54f2d7dd4cdcc7cc
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-10 21:32:10 +0600

    solving matrix

--- function/problem.c
+++ function/problem.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+
+void printArray (int a) {
+	for (int i = 1; i <=a; i++)
+	{
+		/* code */
+		printf("%d ",i);
+	}
+}
+
 int main () {
-	
+	int a;
+	scanf("%d",&a);
+	printArray(a);
 }

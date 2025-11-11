cc898c22b70beaab762f2f322328ee4c3e3bb664
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-11 21:56:24 +0600

    buityfull matrix

--- recursion/main.c
+++ recursion/main.c
@@ -1,19 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+// void printNumbers(int n,int a) {
 
-void printNumbers(int n,int a) {
-
-    if (a > n) {
-        return;
-    } 
-    printNumbers(n,a+1);
-    printf("%d ", a);
+//     if (a > n) {
+//         return;
+//     } 
+//     printNumbers(n,a+1);
+//     printf("%d", a);
+//     if(a != 1)
+//     	printf(" ");
       
     
-}
+// }
+
+// int main() {
+//     int n;
+//     scanf("%d", &n);
+//     printNumbers(n,1);
+//     return 0;
+// }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    printNumbers(n,1);
+    int matrix[5][5];
+    int x, y;
+
+    // Input 5x5 matrix
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            scanf("%d", &matrix[i][j]);
+            if (matrix[i][j] == 1) {
+                x = i + 1; // converting to 1-based index
+                y = j + 1;
+            }
+        }
+    }
+
+    int moves = abs(3 - x) + abs(3 - y);
+    printf("%d\n", moves);
+
     return 0;
 }
+

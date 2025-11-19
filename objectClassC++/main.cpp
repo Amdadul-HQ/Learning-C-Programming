ff679bb65a8ec69467572c079a8495b96bcc54a0
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-19 21:18:00 +0600

    create class and print the object

--- objectClassC++/main.cpp
+++ objectClassC++/main.cpp
@@ -1,10 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+class Student
+{
+ public:
+ char name[100];
+ int roll;
+ double gpa;
+};
 
 int main() {
-	int n;
-	cin >> n;
-	cout << n;
+	Student a;
+	a.roll = 10;
+	a.gpa = 4.5;
+	char tmp[100] = "sakib";
+	strcpy(a.name,tmp);
+
+	cout << a.name << " " << a.roll << " " << a.gpa << endl;
 }

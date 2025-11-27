1847f410a00a2104388fefd08c06d74e1d38d8fe
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-11-27 21:29:16 +0600

    string sorting method

--- c++_string/main.cpp
+++ c++_string/main.cpp
@@ -3,11 +3,35 @@ using namespace std;
 
 int main(){
 
+
+	string s;
+
+	cin >> s ;
+
+	sort(s.begin(),s.end(),greater<char>());
+
+	cout << s ;
+
+
+
+
 	//type one
-	string s = "hello";
+	// string s = "hello";
 
 
 	//type two
 	// string s("hello");
-	cout << s << endl;
+
+	//type three
+	// string s ("hello",3);
+
+	// type four
+	// string s = "hello";
+	// string t(s,3);
+
+	// type five
+	// string s(5,'A');
+
+	// cout << s << endl;
+	// cout << t << endl;
 }

5e1ca2e44a8c5180128d1dacab1d9de0bcb19bfb
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-12-07 16:14:41 +0600

    solving code shef problem

--- dsa_problem_solving/main.cpp
+++ dsa_problem_solving/main.cpp
@@ -14,17 +14,72 @@ using namespace std;
 // }
 
 
-int main(){
+// int main(){
+// 	int T;
+// 	cin >> T;
+// 	// cout << T;
+// 	while(T--){
+// 		int n;
+// 		cin >> n;
+// 		// cout << n << endl;
+// 		double max = 4.0;
+// 		double car = n / max;
+// 		cout << ceil(car) << endl;
+// 	}
+// }
+
+
+// int main() {
+//     int T;
+//     cin >> T;
+
+//     while (T--) {
+//         int N;
+//         cin >> N; 
+//         // cout << N << endl;
+
+//         int rooms = 0; 
+
+//         for (int i = 0; i < N; i++) {
+//             int members;
+//             cin >> members;
+//             rooms += (members + 1) / 2; 
+//         }
+
+//         cout << rooms << endl;
+//     }
+
+//     return 0;
+// }
+
+int main() {
 	int T;
 	cin >> T;
-	// cout << T;
-	while(T--){
-		int n;
-		cin >> n;
-		// cout << n << endl;
-		double max = 4.0;
-		double car = n / max;
-		// cout << car << endl;
-		cout << ceil(car) << endl;
+	while (T--){
+
+	char a[6];
+
+	int flag = 0;
+
+	for(int i = 0 ; i<6 ; i++)
+		{
+			cin >> a[i];
+		}
+	for(int i = 0 ; i<6 ; i++)
+		{
+			// cout << a[i] << " ";
+			if(a[i] == 'W' && a[i + 1] == 'W' && a[i - 1] == 'W')
+			{
+				flag = 1;
+			}
+		}
+	if(flag == 1){
+		cout << "YES";
 	}
+	else {
+		cout << "NO";
+	}
+	cout << endl;
+	}
+	
 }

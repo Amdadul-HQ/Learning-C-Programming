b65b98ac91b7b533caecfdabcf9293258c82dc5d
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-12-19 08:08:40 +0600

    prefix sum proble solve

---
+++ dsa_problem_solving/dsa.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	int preSum[n];
+
+	preSum[0] = a[0];
+
+	for (int i = 1; i < n; i++)
+	{
+		preSum[i] = preSum[i - 1] + a[i];
+		cin >> preSum[i];	
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << preSum[i] << " ";
+	}
+}
